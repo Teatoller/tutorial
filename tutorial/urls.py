b74cea6409daa7ef10d098ac1dc8a378354a4156
steven <steven.otieno@andela.com>
2018-12-27 09:54:00 +0300

    [#162854754] Registers viewsset with router class

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -19,6 +19,10 @@ from django.conf.urls import url, include
 from rest_framework import routers
 from tutorial.quickstart import views
 
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
