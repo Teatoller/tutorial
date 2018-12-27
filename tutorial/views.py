bb145fd15e2bbbd5c75fb79c6516b259ea34af93
steven <steven.otieno@andela.com>
2018-12-27 08:39:27 +0300

    [#162850916] Add endpoint to view and edit users

--- tutorial/views.py
+++ tutorial/views.py
@@ -1,3 +1,8 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from tutorial.quickstart.serializers import UserSerializer, GroupSerializer
+
+class UserViewSets(viewsets.ModelViewSet):
+    ``` API endpoint that allows users to be viewed or edited.```
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = User.Serializer
