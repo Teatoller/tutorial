dfe0f40120fb0ed4eb94bde695890bcd621a8321
Teatoller <steven.otieno1@gmail.com>
2019-01-13 14:35:52 +0300

    [Starts #163176930] Refactor serializer class code

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
-class SnippetSerializer(serializers.Serializer):
+class SnippetSerializer(serializers.ModelSerializer):
     """ Defines field that get serialized/deserialized """
     id = serializers.IntegerField(read_only=True)
     title = serializers.CharField(required=False, allow_blank=True, max_length=100)
