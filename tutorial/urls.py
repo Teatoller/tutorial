218469b3b9b0a65a5e0c68131b09f5a07f65a700
Teatoller <steven.otieno1@gmail.com>
2019-01-14 12:48:54 +0300

    feat(swagger docs): add swagger to installed apps
    - install  django-rest-swagger in virtual environment
    - update requirements.txt
    - add rest_framework_swagger in settings/installed apps
    
    [Starts #163186438]

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -18,6 +18,9 @@ from django.urls import path
 from django.conf.urls import url, include
 from rest_framework import routers
 from tutorial.quickstart import views
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='Pastebin API')
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -26,5 +29,6 @@ router.register(r'groups', views.GroupViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^$', schema_view),
 ]
