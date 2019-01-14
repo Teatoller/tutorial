7f252d1de0febc79375d37043f864be1bca85469
Teatoller <steven.otieno1@gmail.com>
2019-01-14 07:21:57 +0300

    feat(views) implement a simple django view
    - import relevant modules
    - create a method for creating and modifying a snippet
    
    [#163178636]

--- snippets/views.py
+++ snippets/views.py
@@ -5,3 +5,21 @@ from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
+
+@csrf_exempt
+def snippet_list(request):
+    """
+    List all code snippets, or create a new snippet.
+    """
+    if request.method == 'GET':
+        snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(snippets, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
